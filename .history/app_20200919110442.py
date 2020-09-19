f1c9f997f55506677c15829d0a3818c85eaca12f
Bruno Falce <brunofp99@hotmail.com>
2020-09-19 11:29:44 -0300

    change password

---
+++ .history/app_20200919110442.py
@@ -0,0 +1,37 @@
+from flask import Flask, jsonify
+from flask_restful import Api
+from resources.usuario import User, Users, UserRegister, UserLogin, UserLogout, UserUpdate
+from flask_jwt_extended import JWTManager
+from blacklist import BLACKLIST
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///banco.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['JWT_SECRET_KEY'] = 'DontTellAnyone'
+app.config['JWT_BLACKLIST_ENABLED'] = True
+api = Api(app)
+jwt = JWTManager(app)
+
+@app.before_first_request
+def create_bd():
+    banco.create_all()
+
+@jwt.token_in_blacklist_loader
+def verifica_blacklist(token):
+    return token['jti'] in BLACKLIST
+
+@jwt.revoked_token_loader
+def token_de_acesso_invalidado():
+    return jsonify({'message': 'You have been logged out.'}), 401
+
+api.add_resource(Users, '/usuarios')
+api.add_resource(UserLogin, '/usuario/token')
+api.add_resource(UserLogout, '/usuario/delete_token')
+api.add_resource(UserRegister, '/usuario/cadastro')
+api.add_resource(User, '/usuarios/<int:id>')
+api.add_resource(UserUpdate, '/usuario/<int:id>/changed')
+
+if __name__ == '__main__':
+    from sql_alchemy import banco
+    banco.init_app(app)
+    app.run(debug=True)
---
+++ .history/models/usuario_20200919011658.py
@@ -0,0 +1,43 @@
+from sql_alchemy import banco
+
+class UserModel(banco.Model):
+    __tablename__ = 'usuarios'
+
+    id = banco.Column(banco.Integer, primary_key=True, unique=True, nullable=False)
+    login = banco.Column(banco.String(40), nullable=False, unique=True)
+    senha = banco.Column(banco.String(40), nullable=False)
+
+    def __init__(self, login, senha):
+        self.login = login
+        self.senha = senha
+    
+    def json(self):
+        return {
+            'id': self.id,
+            'login': self.login
+        }
+    
+    def save_user(self):
+        banco.session.add(self)
+        banco.session.commit()
+
+    def update_hotel(self, login):
+        self.login = login
+
+    @classmethod
+    def find_user(cls, id):
+        user = cls.query.filter_by(id=id).first()
+        if user:
+            return user
+        return None
+
+    @classmethod
+    def find_by_login(cls, login):
+        user = cls.query.filter_by(login=login).first()
+        if user:
+            return user
+        return None
+
+    def delete_user(self):
+        banco.session.delete(self)
+        banco.session.commit()
